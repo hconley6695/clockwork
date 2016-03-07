bc7961bdaa4991bb57edc3ba975e3ac51c9c2ada
Heather Conley <hconley6695@gmail.com>
2016-03-07 15:11:30 -0500

    completed one formula to create static display, used setInterval to update every second

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var currentHour, currentMinute, currentSecond;
+var currentHour, currentMinute, currentSecond, mornHour;
 
 //var today = newDate();
 
@@ -17,15 +17,25 @@ function formatTime() {
 	currentSecond = today.getSeconds();
 
 	var mornHour = (currentHour < 10 ? "0" : " ") + currentHour;
-	return (mornHour + ":" + currentMinute + ":" + currentSecond);
-}
+	var mornMin = (currentMinute < 10 ? "0" : " ") + currentMinute;
+	var mornSecond = (currentSecond < 10 ? "0" : " ") + currentSecond;
+//return
+var clockDisplay = mornHour + ":" + mornMin + ":" + mornSecond;
+//console.log(clockDisplay);
+//}
 
-var changeTime = setInterval(function() {
-	//return (mornHour + ":" + currentMinute + ":" + currentSecond);
-	return formatTime;
-}, 1000);
+// var changeTime = setInterval(function() 
+// //{
+// 	//return (mornHour + ":" + currentMinute + ":" + currentSecond);
+// 	//return 
+// 	//formatTime;
 
-var workingClock = document.querySelector('.working_time');
-workingClock.textContent = changeTime;
+ 	var workingClock = document.querySelector('.working_time');
+ 	workingClock.textContent = clockDisplay;
+ };
+
+	setInterval(formatTime, 1000);
+// }, 1000);
+
 
 
