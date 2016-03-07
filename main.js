a9f90e3ebda54bbe842afb5a1cd0bc4161be3b7a
Heather Conley <hconley6695@gmail.com>
2016-03-07 13:35:01 -0500

    started building html and working with function to create current time; working on setInterval to change time each second

---
+++ main.js
@@ -0,0 +1,31 @@
+var currentHour, currentMinute, currentSecond;
+
+//var today = newDate();
+
+//currentHour = today.getHours();
+//currentMinute = today.getMinutes();
+//currentSecond = today.getSeconds();
+
+//first-defined what hour, minute, second were.  gave expressions
+//sec--
+
+
+function formatTime() {
+	var today = new Date();
+
+	currentHour = today.getHours();
+	currentMinute = today.getMinutes();
+	currentSecond = today.getSeconds();
+
+currentHour < 10 ? "0" : " "; + currentHour;
+
+}
+
+setInterval(function() {
+
+
+
+} 1000);
+
+
+
